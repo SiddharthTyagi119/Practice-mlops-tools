e1796a7ba7b8f3d814b036f6131d57a66d336b6c
siddharth Tyagi <sidtyagi05@gmail.com>
2024-04-07 15:18:04 +0530

    make dvc data folder

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+Data=[
+    {"name":"sunny","age":28,"city":"bhopal"},
+    {"name":"sudhanshu","age":33,"city":"Delhi"},
+    {"name":"krish","age":35,"city":"banglore"},
+    {"name":"vikas","age":29,"city":"pune"}
+
+
+]
+
+Data=pd.DataFrame(Data)
+pd.DataFrame(Data)
+
+Data.to_csv("data/data.csv",index=False)
